1b46c333aee54e544ba3cb3f0b46839bac4f9656
Bernardomennndes <83621700+Bernardomennndes@users.noreply.github.com>
2022-04-30 09:10:45 -0300

    Context & API:  settled Token in Cookies

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,7 +1,7 @@
-import { createContext, ReactNode, useState } from 'react';
+import { createContext, ReactNode, useEffect, useState } from 'react';
 import { api } from '../services/api';
 import Router from 'next/router';
-import { setCookie } from 'nookies'
+import { setCookie, parseCookies } from 'nookies'
 
 type User = {
     email: string;
@@ -31,6 +31,19 @@ export function AuthProvider({ children }: AuthProviderProps) {
     const [user, setUser] = useState<User>(null);
     const isAuthenticated = !!user;
 
+    useEffect(() => {
+        const { 'quantauth.token': token } = parseCookies();
+
+        if (token) {
+            api.get('/me').then(response => {
+                const { email, permissions, roles } = response.data;
+
+                setUser({email, permissions, roles});
+            })
+        }
+
+    }, [])
+
     async function signIn({ email, password }: SignInCredentials) {
         try {
             const response = await api.post('sessions', {
@@ -54,6 +67,8 @@ export function AuthProvider({ children }: AuthProviderProps) {
                 permissions,
                 roles,
             });
+
+            api.defaults.headers['Authorization'] = `Bearer ${token}`;
 
             Router.push('/dashboard');
 
