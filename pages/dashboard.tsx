200decc2c2e5f4a0a5368b0afc6c8aff6ddb341f
Bernardomennndes <83621700+Bernardomennndes@users.noreply.github.com>
2022-05-01 12:07:49 -0300

    Component: Can (certain permissions to see the component)

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,4 +1,5 @@
 import { useContext, useEffect } from "react";
+import { Can } from "../components/Can";
 import { AuthContext } from "../contexts/AuthContext";
 import { useCan } from "../hooks/useCan";
 import { setupAPIClient } from "../services/api";
@@ -8,8 +9,6 @@ import { withSSRAuth } from "../utils/withSSRAuth";
 export default function Dashboard() {
 
     const { user, isAuthenticated } = useContext(AuthContext)
-
-    const userCanSeeMetrics = useCan({ permissions: ['metrics.list'] });
 
     useEffect(() => {
         api.get('/me')
@@ -22,11 +21,11 @@ export default function Dashboard() {
         <>
             <h1>Dashboard: {user?.email}</h1>
 
-            {userCanSeeMetrics && (
-                <div>
-                     Metrics
-                </div>
-            )}
+            <Can
+                permissions={['metrics.list']}
+            >
+                <div>Metrics</div>
+            </Can>
         </>
     );
 }
